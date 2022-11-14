b1e0ad2c17e938d26396974d1018fd76cdd1a118
nihaalgill <118148719+nihaalgill@users.noreply.github.com>
2022-11-14 21:04:39 +0800

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,1 +1,17 @@
-
+function getBotResponse(input) {
+  //Bobs Responses
+  
+  if (input == "No message sent") {
+    return "Whatever";
+  } else if (input == "Bob") {
+    return "Fine. Be that way";
+  } else if (input.toUpperCase() == input && input.includes('?')){
+    return "Calm down, I know what I'm doing!";
+  } else if (input.toUpperCase() == input){
+    return "Woah,chill out!";
+  } else if (input.includes('?')){
+    return "Sure";
+  }
+}
+  
+  
